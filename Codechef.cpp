9afb70a346ee21f03463582cda8ac9d0b1f18d35
jawhara_1357 <75921118+jawhara2501@users.noreply.github.com>
2021-10-08 22:40:12 -0700

    Create Codechef.cpp

---
+++ Codechef.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,sum=0;
+    cout<<"Enter NO=";
+    cin>>n;
+    for (int i=0; i<n; i++){
+        sum++
+    }
+        cout<<sum<<endl;
+    
+    return 0;
+}
